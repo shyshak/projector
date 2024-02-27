a237844676beb6c389a776d2f64e9e9accc023ab
ashyshak <shyshak.av@gmail.com>
2024-02-27 23:16:57 +0200

    added multiplication

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ def substract_nums(a: float, b: float) -> float:
     return float(a) - float(b)
 
 
+def multuply_nums(a: float, b: float) -> float:
+    return float(a) * float(b)
+
+
 print("""This is a simple calc that can add or substract 2 numbers
       Provide your expression below""")
 user_input = input().replace(" ", "")
@@ -18,3 +22,7 @@ elif '-' in user_input:
     first = user_input[:user_input.index('-')]
     second = user_input[user_input.index('-')+1:]
     print(f'The result of substaction is {substract_nums(first, second)}')
+elif '*' in user_input:
+    first = user_input[:user_input.index('*')]
+    second = user_input[user_input.index('*')+1:]
+    print(f'The result of substaction is {multuply_nums(first, second)}')
